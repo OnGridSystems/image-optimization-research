a5c193c9135d0931d6e8c0d1a6a83321e5d74a45
Kirill Varlamov <kirill@ongrid.pro>
2024-03-16 03:11:23 +0100

    make png-8c

--- convert.sh
+++ convert.sh
@@ -4,11 +4,11 @@ SOURCE_FILES="./Batch1/Images/*.png"
 
 ARGS="-strip -depth 8 -enhance -colors 8"
 
-OUTPUT_FOLDER="./gif-8c"
+OUTPUT_FOLDER="./png-8c"
 
 for file in $(ls $SOURCE_FILES)
 do
   base_name=$(basename "$file" .png)
   echo converting $base_name
-  convert $file $ARGS $OUTPUT_FOLDER/$base_name.gif
+  convert $file $ARGS $OUTPUT_FOLDER/$base_name.png
 done
