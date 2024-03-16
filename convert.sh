c27620e2595db2906c755364d47483ce48accc32
Kirill Varlamov <kirill@ongrid.pro>
2024-03-16 03:08:21 +0100

    make gif-10c

--- convert.sh
+++ convert.sh
@@ -4,11 +4,11 @@ SOURCE_FILES="./Batch1/Images/*.png"
 
 ARGS="-strip -depth 8 -enhance -colors 10"
 
-OUTPUT_FOLDER="./png-10c"
+OUTPUT_FOLDER="./gif-10c"
 
 for file in $(ls $SOURCE_FILES)
 do
   base_name=$(basename "$file" .png)
   echo converting $base_name
-  convert $file $ARGS $OUTPUT_FOLDER/$base_name.png
+  convert $file $ARGS $OUTPUT_FOLDER/$base_name.gif
 done
