4cfcce7e9612d2c0b883d66b8322fab5aff6b4f3
Kirill Varlamov <kirill@ongrid.pro>
2024-03-16 03:15:49 +0100

    make gif-4c

--- convert.sh
+++ convert.sh
@@ -4,11 +4,11 @@ SOURCE_FILES="./Batch1/Images/*.png"
 
 ARGS="-strip -depth 8 -enhance -colors 4"
 
-OUTPUT_FOLDER="./png-4c"
+OUTPUT_FOLDER="./gif-4c"
 
 for file in $(ls $SOURCE_FILES)
 do
   base_name=$(basename "$file" .png)
   echo converting $base_name
-  convert $file $ARGS $OUTPUT_FOLDER/$base_name.png
+  convert $file $ARGS $OUTPUT_FOLDER/$base_name.gif
 done
