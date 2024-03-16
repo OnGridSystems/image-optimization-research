9d3e2ff25bddc068d04a1dcb59c6c2d8664859c3
Kirill Varlamov <kirill@ongrid.pro>
2024-03-16 03:05:15 +0100

    make png-16c

--- convert.sh
+++ convert.sh
@@ -4,11 +4,11 @@ SOURCE_FILES="./Batch1/Images/*.png"
 
 ARGS="-strip -depth 8 -enhance -colors 16"
 
-OUTPUT_FOLDER="./gif-16c"
+OUTPUT_FOLDER="./png-16c"
 
 for file in $(ls $SOURCE_FILES)
 do
   base_name=$(basename "$file" .png)
   echo converting $base_name
-  convert $file $ARGS $OUTPUT_FOLDER/$base_name.gif
+  convert $file $ARGS $OUTPUT_FOLDER/$base_name.png
 done
