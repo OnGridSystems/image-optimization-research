dadd54476ec4dbcad4e76d7108398c7f73842b97
Kirill Varlamov <kirill@ongrid.pro>
2024-03-16 03:09:57 +0100

    make gif-8c

--- convert.sh
+++ convert.sh
@@ -2,9 +2,9 @@
 
 SOURCE_FILES="./Batch1/Images/*.png"
 
-ARGS="-strip -depth 8 -enhance -colors 10"
+ARGS="-strip -depth 8 -enhance -colors 8"
 
-OUTPUT_FOLDER="./gif-10c"
+OUTPUT_FOLDER="./gif-8c"
 
 for file in $(ls $SOURCE_FILES)
 do
