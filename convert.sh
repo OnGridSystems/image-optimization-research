eb338d6110f2a2231898960f0248cb31bd3bd4d0
Kirill Varlamov <kirill@ongrid.pro>
2024-03-16 03:01:34 +0100

    make gif-20c

--- convert.sh
+++ convert.sh
@@ -4,11 +4,11 @@ SOURCE_FILES="./Batch1/Images/*.png"
 
 ARGS="-strip -depth 8 -enhance -colors 20"
 
-OUTPUT_FOLDER="./png-20c"
+OUTPUT_FOLDER="./gif-20c"
 
 for file in $(ls $SOURCE_FILES)
 do
   base_name=$(basename "$file" .png)
   echo converting $base_name
-  convert $file $ARGS $OUTPUT_FOLDER/$base_name.png
+  convert $file $ARGS $OUTPUT_FOLDER/$base_name.gif
 done
