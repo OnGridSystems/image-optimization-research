67f18a6223d92dee20b778e67d42e58b4ff0586b
Kirill Varlamov <kirill@ongrid.pro>
2024-03-19 12:55:33 +0100

    feat: inscribe 2kb image as html page

--- convert.sh
+++ convert.sh
@@ -2,13 +2,13 @@
 
 SOURCE_FILES="./Batch1/Images/*.png"
 
-ARGS="-strip -depth 8 -enhance -colors 4"
+ARGS="-strip -depth 8 -enhance -colors 667"
 
-OUTPUT_FOLDER="./gif-4c"
+OUTPUT_FOLDER="./png-2kb"
 
 for file in $(ls $SOURCE_FILES)
 do
   base_name=$(basename "$file" .png)
   echo converting $base_name
-  convert $file $ARGS $OUTPUT_FOLDER/$base_name.gif
+  convert $file $ARGS $OUTPUT_FOLDER/$base_name.png
 done
